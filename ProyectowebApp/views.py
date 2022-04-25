f6ab125a88a5862a1f6e25e85b5679f45fb6f0b9
Arnovis27 <arnovisdee@hotmail.com>
2022-04-25 08:56:35 -0500

    correciones de carro

--- ProyectowebApp/views.py
+++ ProyectowebApp/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from carroApp.carro import Carro
+
 
 # Create your views here.
 
 def home(request):
+    carro= Carro(request) #iniciar carro desde el inicio para evitar errores posteriores
     return render(request,"ProyectowebApp/home.html")
     
